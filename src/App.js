495352ef396b82da17013ced65928b40814af6e9
Ebrima Tunkara <etunkara51@gmail.com>
2019-02-21 23:09:51 -0500

    Change some colors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import QuoteCard from './components/QuoteCard'
 
 
 class App extends Component {
-  render() {
-    return (
-      <div id="container">
-          <QuoteCard/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="container">
+                <QuoteCard/>
+            </div>
+
+        );
+    }
 }
 
 export default App;
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import '../styles/styles.css'
+
 class QuoteCard extends React.Component {
 
     render() {
         return (
-            <div className="card"></div>
+            <div className="card">This is a Quote Card</div>
         )
 
     }
