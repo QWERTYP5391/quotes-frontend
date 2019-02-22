2b22df7f560c6da1255c7f2f83b285e572f93818
Ebrima Tunkara <etunkara51@gmail.com>
2019-02-21 22:53:58 -0500

    start with a simple card to hold quote information

---
+++ src/components/QuoteCard.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import '../styles/styles.css'
+class QuoteCard extends React.Component {
+
+    render() {
+        return (
+            <div className="card"></div>
+        )
+
+    }
+
+
+}
+
+export default QuoteCard;
